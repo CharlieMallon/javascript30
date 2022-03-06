3cae37db6e8cb58ad039679c7d1a6bdddd340622
CharlieMallon <penguin47@hotmail.co.uk>
2022-03-06 20:13:09 +0000

    split js into seprate file

---
+++ 03-CSSVariables/cssVariables.js
@@ -0,0 +1,13 @@
+// gets all the inputs
+const inputs = document.querySelectorAll('.controls input')
+
+function handleUpdate(){
+  // gets the suffix from the data attribute on the input
+  const suffix = this.dataset.sizing || '';
+  // updates the style property using the elements name, value and suffix
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+}
+
+//  for each of the inputs add an event listener
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
