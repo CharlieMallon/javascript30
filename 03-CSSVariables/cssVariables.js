e72e6774a3a946a4f047489ec44d154266bde9b7
CharlieMallon <penguin47@hotmail.co.uk>
2022-03-06 20:16:13 +0000

    split js into seprate file

--- 03-CSSVariables/cssVariables.js
+++ 03-CSSVariables/cssVariables.js
@@ -3,9 +3,9 @@ const inputs = document.querySelectorAll('.controls input')
 
 function handleUpdate(){
   // gets the suffix from the data attribute on the input
-  const suffix = this.dataset.sizing || '';
+    const suffix = this.dataset.sizing || '';
   // updates the style property using the elements name, value and suffix
-  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
 }
 
 //  for each of the inputs add an event listener
